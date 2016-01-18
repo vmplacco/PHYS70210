5a9c9c68749470857282e5828cd35b5bf8cbc8cd
Vinicius Placco <vplacco@nd.edu>
2016-01-18 15:42:34 -0500

    add query example

---
+++ psql_intro/query_pipeline.sql
@@ -0,0 +1,23 @@
+\echo ''
+
+\pset format 'unaligned'
+\pset null "-9.999"
+\pset fieldsep '\t'
+\pset footer
+\pset tuples_only
+\pset title
+
+\echo ''
+
+\o /emc1/home/vplacco/PHYS70210/psql_intro/table1.dat
+
+SELECT spspec1,feh_biw,teff_adop,cfe,j0,ra,dec
+FROM pipeline 
+WHERE color='JK' AND teff_adop<7000;
+
+\o
+	
+\pset footer
+\pset tuples_only
+\r
+\echo ''
