0f91ba4b77977854c934d0f120b5b77d83094c1f
Vinicius Placco <vplacco@nd.edu>
2016-02-10 15:57:46 -0500

    add r example to read csv into psql

--- r_psql/r_nsspp.r
+++ r_psql/r_nsspp.r
@@ -1,9 +1,9 @@
 library(RPostgreSQL)
 
 drv <- dbDriver("PostgreSQL")
-con <- dbConnect(drv, host="localhost", port="5432", dbname="hes")
+con <- dbConnect(drv, host="localhost", port="5432", dbname="dbname")
 
-x <- data.frame(read.table("ANN5TIE.CSV",sep=",",header=TRUE))
+x <- data.frame(read.table("FILE.CSV",sep=",",header=TRUE))
 dbWriteTable(con,"table3", x)
 
 dbGetQuery(con, "ALTER TABLE table3 ADD COLUMN id SERIAL PRIMARY KEY;")
